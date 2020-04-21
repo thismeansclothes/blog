5763dcbdc08e404d5e1b4e891a1603858bb77644
Uisang Lee <dmltkdzlem@naver.com>
2020-04-21 18:09:54 +0900

    blog

--- secondproject/secondproject/urls.py
+++ secondproject/secondproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blog.views.home, name='home'),
+    path('blog/<int:blog_id>/', blog.views.detail, name ='detail'),
+    path('blog/new/', blog.views.new, name='new'),
+    path('blog/create/', blog.views.create, name = 'create'),
 ]
